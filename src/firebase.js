593b4170bb6b40c04cc99d9be460df06544ff909
Nazish Fraz <nazish@mildlyclassic.com>
2019-03-22 16:43:48 +0530

    modified firebase file

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import firestore from 'firebase/firestore'
 const settings = {timestampsInSnapshots: true};
 
 const config = {
-    apiKey: "AIzaSyCaf9YPdsMY2Ig_j2vWbpl9EVgVOOvkKWk",
-    authDomain: "gouherdanish-d91ea.firebaseapp.com",
-    databaseURL: "https://gouherdanish-d91ea.firebaseio.com",
-    projectId: "gouherdanish-d91ea",
-    storageBucket: "gouherdanish-d91ea.appspot.com",
-    messagingSenderId: "811281262218"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 const db = firebase.initializeApp(config).firestore();
 
